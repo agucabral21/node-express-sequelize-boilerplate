f57a928e99aec677e0a76a577e2bec40bf025c11
agus <agucabral@gmail.com>
2023-02-15 16:35:11 -0300

    feature : added winston and morgan

--- src/config/app.js
+++ src/config/app.js
@@ -5,11 +5,13 @@ dotenv.config();
 
 const express = require('express');
 const APIv1 = require('../routes');
+const { morgan } = require('../middlewares');
 
 const app = express();
 
 app.set('json spaces', 2);
 app.use(express.json());
+app.use(morgan);
 
 app.use('/api/v1', APIv1);
 
