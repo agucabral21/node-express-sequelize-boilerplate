2570f3f106b548af3783b2041c1d8466ebbe737d
agus <agucabral@gmail.com>
2023-02-17 17:02:30 -0300

    feature: added schema validaton middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,9 +1,11 @@
 const urlNotFound = require('./urlNotFound');
 const morgan = require('./morgan');
 const errorHandler = require('./errorHandler');
+const schemaValidator = require('./schemaValidator');
 
 module.exports = {
   urlNotFound,
   morgan,
   errorHandler,
+  schemaValidator,
 };
