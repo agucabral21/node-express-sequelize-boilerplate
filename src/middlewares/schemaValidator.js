ed145f77925c2cc6e57eb80db77acf18e267e72d
agus <agucabral@gmail.com>
2023-02-17 17:02:30 -0300

    feature: added schema validaton middleware

---
+++ src/middlewares/schemaValidator.js
@@ -0,0 +1,18 @@
+const { checkSchema, validationResult } = require('express-validator');
+const { errorResponse } = require('../utils/responses');
+
+const schemaValidator = (schema) => async (req, res, next) => {
+  // Run schema validation
+  const validations = checkSchema(schema);
+  await Promise.all(validations.map((validation) => validation.run(req)));
+  // Check errors
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  const message = 'Invalid data Found';
+
+  return res.status(400).json(errorResponse({ message, errors }));
+};
+
+module.exports = schemaValidator;
