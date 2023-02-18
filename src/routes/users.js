dbc0d4990f13c5f7d3f8fa16183aa912f574fe71
agus <agucabral@gmail.com>
2023-02-17 23:39:00 -0300

    feature: added validation to addRole endpoint

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
 const { UserController } = require("../controllers");
 const { catchAsync } = require("../utils");
+const { schemaValidator } = require("../middlewares");
+const { add } = require("../validation/user");
 
 router.post("/", catchAsync(UserController.add));
-router.post("/:id/roles", catchAsync(UserController.addRoles));
+router.post(
+  "/:id/roles",
+  schemaValidator(add),
+  catchAsync(UserController.addRoles)
+);
 
 module.exports = router;
