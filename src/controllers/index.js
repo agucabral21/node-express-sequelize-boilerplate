2c4b113d7da2a4cd26fd1f567ae8e9471a130e52
agus <agucabral@gmail.com>
2023-02-21 23:21:12 -0300

    feature: added auth endpoint

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,11 @@
 const StatusController = require("./StatusController");
 const UserController = require("./UserController");
 const RoleController = require("./RoleController");
+const AuthController = require("./AuthController");
 
-module.exports = { StatusController, UserController, RoleController };
+module.exports = {
+  StatusController,
+  UserController,
+  RoleController,
+  AuthController,
+};
--- src/validation/role/create.js
+++ src/validation/role/create.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-plusplus */
 module.exports = {
   name: {
     in: ["body"],
